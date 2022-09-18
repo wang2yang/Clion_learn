91732b0bf27d79f665daaba926b259e97a63995d
wang2yang <1902724205@qq.com>
2022-09-18 23:59:15 +0800

    project initial

---
+++ cmake-build-debug/CMakeFiles/Clion_learn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clion_learn.dir/main.cpp.obj"
+  "Clion_learn.exe"
+  "Clion_learn.exe.manifest"
+  "Clion_learn.pdb"
+  "libClion_learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clion_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wangYang on 2022/9/18.
+//
+
+#include<iostream>
+
+int main(void){
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
